450322608f130d762bdfd7bdd179456070907495
dario <dario.albelo@hotmail.com>
2023-09-08 01:25:06 -0300

    modifico el DTO de usuario

--- src/main/kotlin/com/tramitarte/proyecto/dominio/UpdateUserDTO.kt
+++ src/main/kotlin/com/tramitarte/proyecto/dominio/UpdateUserDTO.kt
@@ -3,8 +3,8 @@ package com.tramitarte.proyecto.dominio
 import java.time.LocalDate
 
 data class UpdateUserDTO(
-        var name: String,
-        var surname: String,
+        //var name: String,
+        //var surname: String,
         var username: String,
-        var birthdate: LocalDate
+        //var birthdate: LocalDate
 )
